c5666c1a84b7c90cc5a6ed8051bea93da3945342
Z3R0GT <117790541+Z3R0GT@users.noreply.github.com>
2023-06-01 19:38:33 -0500

    BUILD 4.0
    Four version of WC.
    
    contains:
    -- priv func by errors
    -- optimized code, V1.0
    -
    -- Func:
        - edits text
        - nodes (button, label, text, entry, listbox, option menu of tkinter)
        - images custom with PIL
    -- Procces:
        - custom fonts
        - others...
    -- System:
        - save data and load that
    
    this version is a copy of before version (BUILD 2.0) with different sintax
    
    Thanks for read!

---
+++ BUILD 4.0/testa.py
@@ -0,0 +1,26 @@
+from WCPriv import *
+
+while True:
+    a = input("CONSULTA \n")
+
+    if a == "VARIABLES":
+        print(WC.DEV.DEVCONSULT())
+
+    elif a == "SETDIR":
+        B = input("DIRECCION: \n ")
+        WC.Config.set_dir(DIR=B)
+
+    elif a == "SAVE":    
+
+        data = {
+            "Nombre": "jose",
+            "edad": "13",
+            "apellido": "uwu"
+        }
+
+        WC.System.save_archive(data=data, ID_FOLDER=int(input("ID FOLDER: ")), name_archive=input("Nombre de archivo: \n"))
+    elif a == "LOAD":
+        pass
+
+    elif a == "EXIT":
+        break
